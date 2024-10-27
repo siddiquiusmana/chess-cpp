cc9c5a47d5840b4c6ba31c99fcf507f38973e07b
Usman Ali Siddiqui <forgamesandservices@gmail.com>
2024-10-27 16:22:25 -0400

    Test spdlogger
    Set up a quick example of spdlog for logging purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "include/SDLWindow.h"
+#include <spdlog/cfg/env.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 int main(void)
 {
+    spdlog::cfg::load_env_levels();
+
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::debug("This is a debug message");
+
     // Create a window
     SDLWindow window("Chess");
     window.createWindow();
